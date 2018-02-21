c8438fb4d99c19dd5b8f87c4d070648e8e547ecf
LouiseW524 <louise.m.walsh@mycit.ie>
2018-02-21 14:23:42 +0000

    changing back

--- DataGatheringBatch.bat
+++ DataGatheringBatch.bat
@@ -3,4 +3,4 @@ title DataGathering
 :: Accessing API and Downloading dataset
 cd C:/Users/louis/AppData/Local/Programs/Python/Python36-32/Scripts && kaggle config path -p C:/"Program Files (x86)"/Jenkins/workspace/"Fourth Year Project V1.1" && kaggle datasets download -d shubhmamp/english-premier-league-match-data
 :: Unzipping the dataset
-cd C:/"Program Files (x86)"/Jenkins/workspace/"Fourth Year Project V1.1"/datasets/shubhmamp/english-premier-league-match-data && dir &&  C:/"Program Files"/7-Zip/7z x english-premier-league-match-data.zip
+cd C:/"Program Files (x86)"/Jenkins/workspace/"Fourth Year Project V1.1"/datasets/shubhmamp/english-premier-league-match-data && dir &&  C:/"Program Files"/7-Zip/7z x datafile.zip
