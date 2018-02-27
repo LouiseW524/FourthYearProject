45610ca44ad43783ccb6a5471908b9372dbefed7
LouiseW524 <louise.m.walsh@mycit.ie>
2018-02-27 16:54:17 +0000

    mongo imports

--- MongoImports.bat
+++ MongoImports.bat
@@ -1,15 +1,16 @@
 echo on
 title MongoSetup
 ::Importing mongodb match JSON files into respective collections
-C:/"Program Files (x86)"/Jenkins/tools/org.jenkinsci.plugins.mongodb.MongoDBInstallation/mongodb/bin/mongoimport --db soccerStats --collection matchStats --jsonArray --file season_match_stats14-15.json
-C:/"Program Files (x86)"/Jenkins/tools/org.jenkinsci.plugins.mongodb.MongoDBInstallation/mongodb/bin/mongoimport --db soccerStats --collection matchStats --jsonArray --file season_match_stats15-16.json
-C:/"Program Files (x86)"/Jenkins/tools/org.jenkinsci.plugins.mongodb.MongoDBInstallation/mongodb/bin/mongoimport --db soccerStats --collection matchStats --jsonArray --file season_match_stats16-17.json
-C:/"Program Files (x86)"/Jenkins/tools/org.jenkinsci.plugins.mongodb.MongoDBInstallation/mongodb/bin/mongoimport --db soccerStats --collection matchStats --jsonArray --file season_match_stats17-18.json	
+cd C:/"Program Files (x86)"/Jenkins/tools/org.jenkinsci.plugins.mongodb.MongoDBInstallation/mongodb/bin
+mongoimport --db soccerStats --collection matchStats --file season_match_stats14-15.json
+mongoimport --db soccerStats --collection matchStats --file season_match_stats15-16.json
+mongoimport --db soccerStats --collection matchStats --file season_match_stats16-17.json
+mongoimport --db soccerStats --collection matchStats --file season_match_stats17-18.json	
 ::Importing mongodb player JSON files into respective collections
-C:/"Program Files (x86)"/Jenkins/tools/org.jenkinsci.plugins.mongodb.MongoDBInstallation/mongodb/bin/mongoimport --db soccerStats --collection playerStats --jsonArray --file season_stats14-15.json
-C:/"Program Files (x86)"/Jenkins/tools/org.jenkinsci.plugins.mongodb.MongoDBInstallation/mongodb/bin/mongoimport --db soccerStats --collection playerStats --jsonArray --file season_stats15-16.json
-C:/"Program Files (x86)"/Jenkins/tools/org.jenkinsci.plugins.mongodb.MongoDBInstallation/mongodb/bin/mongoimport --db soccerStats --collection playerStats --jsonArray --file season_stats16-17.json
-C:/"Program Files (x86)"/Jenkins/tools/org.jenkinsci.plugins.mongodb.MongoDBInstallation/mongodb/bin/mongoimport --db soccerStats --collection playerStats --jsonArray --file season_stats17-18.json
+mongoimport --db soccerStats --collection playerStats --file season_stats14-15.json
+mongoimport --db soccerStats --collection playerStats --file season_stats15-16.json
+mongoimport --db soccerStats --collection playerStats --file season_stats16-17.json
+mongoimport --db soccerStats --collection playerStats --file season_stats17-18.json
 
 		
 		
