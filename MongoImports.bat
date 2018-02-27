bb6932eb50043cceb243669038c743f043929b5f
LouiseW524 <louise.m.walsh@mycit.ie>
2018-02-27 16:58:23 +0000

    mongo imports

--- MongoImports.bat
+++ MongoImports.bat
@@ -1,16 +1,17 @@
 echo on
 title MongoSetup
 ::Importing mongodb match JSON files into respective collections
+STR="C:/Program Files (x86)/Jenkins/workspace/Fourth Year Project V1.1/datasets"
 cd C:/"Program Files (x86)"/Jenkins/tools/org.jenkinsci.plugins.mongodb.MongoDBInstallation/mongodb/bin
-mongoimport --db soccerStats --collection matchStats --file season_match_stats14-15.json
-mongoimport --db soccerStats --collection matchStats --file season_match_stats15-16.json
-mongoimport --db soccerStats --collection matchStats --file season_match_stats16-17.json
-mongoimport --db soccerStats --collection matchStats --file season_match_stats17-18.json	
+mongoimport --db soccerStats --collection matchStats --file $STR/season_match_stats14-15.json
+mongoimport --db soccerStats --collection matchStats --file $STR/season_match_stats15-16.json
+mongoimport --db soccerStats --collection matchStats --file $STR/season_match_stats16-17.json
+mongoimport --db soccerStats --collection matchStats --file $STR/season_match_stats17-18.json	
 ::Importing mongodb player JSON files into respective collections
-mongoimport --db soccerStats --collection playerStats --file season_stats14-15.json
-mongoimport --db soccerStats --collection playerStats --file season_stats15-16.json
-mongoimport --db soccerStats --collection playerStats --file season_stats16-17.json
-mongoimport --db soccerStats --collection playerStats --file season_stats17-18.json
+mongoimport --db soccerStats --collection playerStats --file $STR/season_stats14-15.json
+mongoimport --db soccerStats --collection playerStats --file $STR/season_stats15-16.json
+mongoimport --db soccerStats --collection playerStats --file $STR/season_stats16-17.json
+mongoimport --db soccerStats --collection playerStats --file $STR/season_stats17-18.json
 
 		
 		
