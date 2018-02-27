15678217885e19e9601979c7409ed6b186afa3a8
LouiseW524 <louise.m.walsh@mycit.ie>
2018-02-27 15:37:41 +0000

    Fixing output directory for python files

--- JSONCleanerLine.py
+++ JSONCleanerLine.py
@@ -1,7 +1,7 @@
 import json
 import os
 import glob
-
+year = 14
 for filename in glob.iglob('**/*.json', recursive=True):
      print(filename)
      with open(filename, encoding="utf8") as json_file:
@@ -9,5 +9,5 @@ for filename in glob.iglob('**/*.json', recursive=True):
         year = year + 1
         for line in data.split():
             line.replace(".", " ")
-     f = open("C:/Program Files (x86)/Jenkins/workspace/data/MongoFiles" + filename , "w")
+     f = open("C:/Program Files (x86)/Jenkins/workspace/data/MongoFiles" , "w")
      f.write(data)
