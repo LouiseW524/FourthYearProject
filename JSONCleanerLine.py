1dbd8f09ef41faf5ac9624e8d50a9f138a0fca22
LouiseW524 <louise.m.walsh@mycit.ie>
2018-02-27 15:46:38 +0000

    removing unused code

--- JSONCleanerLine.py
+++ JSONCleanerLine.py
@@ -1,13 +1,12 @@
 import json
 import os
 import glob
-year = 14
+
 for filename in glob.iglob('**/*.json', recursive=True):
      print(filename)
      with open(filename, encoding="utf8") as json_file:
         data = str(json.load(json_file))
-        year = year + 1
         for line in data.split():
             line.replace(".", " ")
-     f = open("C:/Program Files (x86)/Jenkins/workspace/data/MongoFiles" , "w")
+     f = open(filename , "w")
      f.write(data)
