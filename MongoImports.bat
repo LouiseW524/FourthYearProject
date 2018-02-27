d2fb381ef6d49802b917bd088df5aa410cac409c
LouiseW524 <louise.m.walsh@mycit.ie>
2018-02-27 17:10:15 +0000

    deleting db command

--- MongoImports.bat
+++ MongoImports.bat
@@ -1,6 +1,7 @@
 echo on
 title MongoSetup
 ::Importing mongodb match JSON files into respective collections
+mongo soccerStats --eval "db.dropDatabase()"
 cd C:/"Program Files (x86)"/Jenkins/tools/org.jenkinsci.plugins.mongodb.MongoDBInstallation/mongodb/bin
 mongoimport --db soccerStats --collection matchStats --file C:/"Program Files (x86)"/Jenkins/workspace/"Fourth Year Project V1.1"/datasets/season_match_stats14-15.json
 mongoimport --db soccerStats --collection matchStats --file C:/"Program Files (x86)"/Jenkins/workspace/"Fourth Year Project V1.1"/datasets/season_match_stats15-16.json
