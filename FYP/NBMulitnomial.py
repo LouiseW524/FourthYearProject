e7b010b56ebe065fd86cddad424aa9f1eff3cb60
LouiseW524 <louise.m.walsh@mycit.ie>
2018-04-16 14:45:41 +0100

    parameterised build jenkins

--- FYP/NBMulitnomial.py
+++ FYP/NBMulitnomial.py
@@ -1,6 +1,13 @@
 #!/usr/bin/python
+import psycopg2
+from login_details import DB_PASSWORD, DB_USER
+conn = psycopg2.connect("dbname='fyp' user=%s host='localhost' password=%s" % (DB_USER, DB_PASSWORD))
+conn.set_isolation_level(0)
+cur = conn.cursor()
 
 import sys
 
-print('Number of arguments:', len(sys.argv), 'arguments.')
-print('Argument List:', str(sys.argv))
+#cur.execute("""SELECT * FROM teamlist where matchid = %s """, (match_id, team_id))
+#conceded = cur.fetchall()
+
+print('Argument List:', sys.argv[1])
